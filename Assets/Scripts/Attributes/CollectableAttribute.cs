748a0636e827dfc2f70c485c1f77d4d34551f6d2
pauldubois98 <pauldubois98@gmail.com>
2017-10-11 00:18:33 +0200

    added the unity project

---
+++ Assets/Scripts/Attributes/CollectableAttribute.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class CollectableAttribute : MonoBehaviour
+{
+	private UIScript userInterface;
+
+
+	// Start is called at the beginning
+	private void Start()
+	{
+		// Find the UI in the scene and store a reference for later use
+		userInterface = GameObject.FindObjectOfType<UIScript>();
+	}
+
+
+
+	// This function gets called everytime this object collides with another
+	private void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		string playerTag = otherCollider.gameObject.tag;
+
+		// is the other object a player?
+		if(playerTag == "Player" || playerTag == "Player2")
+		{
+			if(userInterface != null)
+			{
+				// add one point
+				int playerId = (playerTag == "Player") ? 0 : 1;
+				userInterface.AddOnePoint(playerId);
+			}
+
+			// then destroy this object
+			gameObject.GetComponent<Collider2D>().enabled = false;
+
+			GameObject.FindGameObjectsWithTag("gate")[0].GetComponent<Collider2D>().enabled = false;
+			GameObject.FindGameObjectsWithTag("gate")[0].GetComponent<Renderer>().material.color = Color.red;
+		}
+	}
+
+
+
+
+	// This function gets called everytime this object collides with another
+	private void OnTriggerExit2D(Collider2D otherCollider)
+	{
+		string playerTag = otherCollider.gameObject.tag;
+
+		// is the other object a player?
+		if(playerTag == "Player" || playerTag == "Player2")
+		{
+			if(userInterface != null)
+			{
+				// add one point
+				int playerId = (playerTag == "Player") ? 0 : 1;
+				userInterface.AddOnePoint(playerId);
+			}
+
+			// then destroy this object
+			gameObject.GetComponent<Collider2D>().enabled = true;
+
+			GameObject.FindGameObjectsWithTag ("gate") [0].GetComponent<Collider2D>().enabled = true;
+			GameObject.FindGameObjectsWithTag("gate")[0].GetComponent<Renderer>().material.color = Color.grey;
+
+		}
+	}
+}
